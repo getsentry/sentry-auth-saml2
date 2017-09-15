8f9cc19ca8daa23f529d6f0003999202b24c6c68
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-09-15 13:23:04 -0700

    feat(merge-providers): Import OneLogin provider

---
+++ src/sentry_auth_saml2/onelogin/provider.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import, print_function
+
+from sentry.auth.providers.saml2 import SAML2Provider
+
+from .views import (
+    OneLoginSAML2ConfigureView, SelectIdP
+)
+
+from .constants import ONELOGIN_EMAIL, ONELOGIN_FIRSTNAME
+
+
+class OneLoginSAML2Provider(SAML2Provider):
+    name = 'OneLogin'
+
+    def get_configure_view(self):
+        return OneLoginSAML2ConfigureView.as_view()
+
+    def get_setup_pipeline(self):
+        return [
+            SelectIdP()
+        ]
+
+    def build_config(self, state):
+        data = super(OneLoginSAML2Provider, self).build_config(state)
+
+        if data:
+            data['attribute_mapping'] = {
+                'attribute_mapping_email': ONELOGIN_EMAIL,
+                'attribute_mapping_firstname': ONELOGIN_FIRSTNAME
+            }
+        return data
