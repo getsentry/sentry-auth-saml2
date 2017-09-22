46b2cc3310e3820f433637d1a0c8dd22357b3d0c
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-09-22 15:05:55 -0700

    ref: Pull generic saml form logic out of each plugin

--- src/sentry_auth_saml2/generic/views.py
+++ src/sentry_auth_saml2/generic/views.py
@@ -1,14 +1,48 @@
 from __future__ import absolute_import, print_function
 
-from django import forms
-from onelogin.saml2.idp_metadata_parser import OneLogin_Saml2_IdPMetadataParser
+from django.core.urlresolvers import reverse
 
 from sentry.auth.view import AuthView, ConfigureView
 from sentry.auth.providers.saml2 import SAML2Provider
+from sentry.utils.http import absolute_uri
+
+from sentry_auth_saml2.forms import (
+    SAMLForm,
+    URLMetadataForm,
+    XMLMetadataForm,
+    process_metadata,
+)
+
+
+class SelectIdP(AuthView):
+    def handle(self, request, helper):
+        op = 'url'
+
+        forms = {
+            'url': URLMetadataForm(),
+            'xml': XMLMetadataForm(),
+            'idp': SAMLForm(),
+        }
+
+        if 'action_save' in request.POST:
+            op = request.POST['action_save']
+            form_cls = forms[op].__class__
+            forms[op] = process_metadata(form_cls, request, helper)
+
+        # process_metadata will return None when the action was successful and
+        # data was bound to the helper.
+        if not forms[op]:
+            return helper.next_step()
+
+        return self.respond('sentry_auth_saml2/select-idp.html', {
+            'op': op,
+            'forms': forms,
+        })
 
 
 class GenericSAML2ConfigureView(ConfigureView):
     def dispatch(self, request, organization, auth_provider):
+        sp_metadata_url = absolute_uri(reverse('sentry-auth-organization-saml-metadata', args=[organization.slug]))
         data = auth_provider.config['idp']
         if request.POST:
             data = request.POST
@@ -21,70 +55,6 @@ class GenericSAML2ConfigureView(ConfigureView):
             form = SAMLForm(data)
 
         return self.render('sentry_auth_saml2/configure.html', {
+            'sp_metadata_url': sp_metadata_url,
             'form': form
-        })
-
-
-class SAMLForm(forms.Form):
-    idp_entityid = forms.CharField(label='IdP Entity ID')
-    idp_sso_url = forms.URLField(label='IdP Single Sign On URL')
-    idp_slo_url = forms.URLField(label='IdP Single Log Out URL', required=False)
-    idp_x509cert = forms.CharField(label='IdP x509 public certificate', widget=forms.Textarea)
-
-
-class SelectIdPForm(SAMLForm):
-    provider = forms.CharField(widget=forms.HiddenInput, initial='saml2', required=False)
-
-
-class SelectIdP(AuthView):
-    def handle(self, request, helper):
-        error_url = error_xml = False
-        url = xml = ''
-        op = 'url'
-
-        if 'action_save' in request.POST:
-            op = 'idp'
-            form = SelectIdPForm(request.POST)
-            if form.is_valid():
-                idp_data = SAML2Provider.extract_idp_data_from_form(form)
-
-                helper.bind_state('idp', idp_data)
-                helper.bind_state('contact', request.user.email)
-                return helper.next_step()
-        else:
-            form = SelectIdPForm()
-            if 'action_get_metadata' in request.POST or 'action_parse_metadata' in request.POST:
-                if 'action_get_metadata' in request.POST:
-                    op = 'url'
-                    url = request.POST['idp_metadata_url']
-                    error_url = True
-                    if url:
-                        try:
-                            data = OneLogin_Saml2_IdPMetadataParser.parse_remote(url)
-                        except:
-                            data = None
-                else:
-                    op = 'xml'
-                    xml = request.POST['idp_metadata_xml']
-                    error_xml = True
-                    if xml:
-                        try:
-                            data = OneLogin_Saml2_IdPMetadataParser.parse(xml)
-                        except:
-                            data = None
-                if data and 'idp' in data:
-                    idp_data = SAML2Provider.extract_idp_data_from_parsed_data(data)
-                    form2 = SAMLForm(idp_data)
-                    if form2.is_valid():
-                        helper.bind_state('idp', idp_data)
-                        helper.bind_state('contact', request.user.email)
-                        return helper.next_step()
-
-        return self.respond('sentry_auth_saml2/select-idp.html', {
-            'op': op,
-            'form': form,
-            'error_url': error_url,
-            'url': url,
-            'error_xml': error_xml,
-            'xml': xml
         })
--- src/sentry_auth_saml2/onelogin/views.py
+++ src/sentry_auth_saml2/onelogin/views.py
@@ -1,28 +1,17 @@
 from __future__ import absolute_import, print_function
 
 from django import forms
-from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
-from django.core.validators import URLValidator
 from django.utils.translation import ugettext_lazy as _
-
-from onelogin.saml2.idp_metadata_parser import OneLogin_Saml2_IdPMetadataParser
 
 from sentry.auth.view import AuthView
 from sentry.auth.providers.saml2 import (
-    SAML2Provider,
     SAML2ConfigureView as BaseSAML2ConfigureView,
     NAMEID_FORMAT_CHOICES,
     AUTHNCONTEXT_CHOICES
 )
 from sentry.utils.http import absolute_uri
-
-
-class SAMLForm(forms.Form):
-    idp_entityid = forms.CharField(label='OneLogin Entity ID', help_text="Issuer URL")
-    idp_sso_url = forms.URLField(label='OneLogin Single Sign On URL', help_text="SAML 2.0 Endpoint (HTTP)")
-    idp_slo_url = forms.URLField(label='OneLogin Single Log Out URL', required=False, help_text="SLO Endpoint (HTTP)")
-    idp_x509cert = forms.CharField(label='OneLogin x509 public certificate', widget=forms.Textarea, help_text="X.509 Certificate")
+from sentry_auth_saml2.generic.views import SAMLForm, URLMetadataForm, process_metadata
 
 
 class AdvancedForm(forms.Form):
@@ -93,32 +82,9 @@ class OneLoginSAML2ConfigureView(BaseSAML2ConfigureView):
 
 class SelectIdP(AuthView):
     def handle(self, request, helper):
-        error_value = error_url = False
-        metadata_url = url = ''
-        if 'action_save' in request.POST:
-            metadata_url = request.POST['metadata_url']
+        form = process_metadata(URLMetadataForm, request, helper)
 
-            validate_url = URLValidator()
-            try:
-                validate_url(metadata_url)
-                url = metadata_url
-                try:
-                    data = OneLogin_Saml2_IdPMetadataParser.parse_remote(url)
-
-                    if data and 'idp' in data:
-                        idp_data = SAML2Provider.extract_idp_data_from_parsed_data(data)
-                        form2 = SAMLForm(idp_data)
-                        if form2.is_valid():
-                            helper.bind_state('idp', idp_data)
-                            helper.bind_state('contact', request.user.email)
-                            return helper.next_step()
-                except Exception:
-                    error_url = True
-            except ValidationError:
-                error_value = True
-
-        return self.respond('sentry_auth_onelogin/select-idp.html', {
-            'error_value': error_value,
-            'error_url': error_url,
-            'metadata_url': metadata_url
-        })
+        if form:
+            return self.respond('sentry_auth_onelogin/select-idp.html', {'form': form})
+        else:
+            return helper.next_step()
