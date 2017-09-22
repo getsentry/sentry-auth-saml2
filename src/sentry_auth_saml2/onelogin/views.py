81857ed1986b27427f62b808b54392a579fb77c5
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-09-22 16:25:42 -0700

    ref: Render OneLogin idp selector with crispy

--- src/sentry_auth_saml2/onelogin/views.py
+++ src/sentry_auth_saml2/onelogin/views.py
@@ -80,9 +80,14 @@ class OneLoginSAML2ConfigureView(BaseSAML2ConfigureView):
         })
 
 
+# Onelogin specifically cals their Metadata URL a 'Issuer URL'
+class OneLoginURLMetadataForm(URLMetadataForm):
+    metadata_url = forms.URLField(label='Issuer URL')
+
+
 class SelectIdP(AuthView):
     def handle(self, request, helper):
-        form = process_metadata(URLMetadataForm, request, helper)
+        form = process_metadata(OneLoginURLMetadataForm, request, helper)
 
         if form:
             return self.respond('sentry_auth_onelogin/select-idp.html', {'form': form})
