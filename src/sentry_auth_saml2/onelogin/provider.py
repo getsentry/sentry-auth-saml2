09e4d9b79ed2c5b331f1c989d6979e7013302a64
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-09-27 18:02:11 -0700

    ref: Update providers for the new SAML2Provider interface
    This follows up https://github.com/getsentry/sentry/pull/6206
    
     - The generic provider now has an additional configuration step that
       was necessitated by the correction to require an identifier *and*
       email address, where as before we were simply just using the email as
       provider ID.
    
       During SAML2 provider setup you will now be prompted to map the IdP
       attribute keys.
    
     - All providers should now implement the get_saml_setup_pipeline,
       instead of get_setup_pipeline. This allows the base provider to
       implement get_setup_pipeline correctly (It combines the
       authentication step)
    
     - OneLogin no longer has additional configuration options ("advanced
       options"). There is no reason the user would end up needing to change
       these once the SSO link has been setup.
    
       In the future if we *really* need to expose SAML type configuration
       for hyper-specific SAML providers, we can easily do this.
    
       The generic provider still allows re-configuration of SAML options.
    
     - Specify OneLogin attribute mapping, remove constants file for this.

--- src/sentry_auth_saml2/onelogin/provider.py
+++ src/sentry_auth_saml2/onelogin/provider.py
@@ -1,31 +1,20 @@
 from __future__ import absolute_import, print_function
 
-from sentry.auth.providers.saml2 import SAML2Provider
+from sentry.auth.providers.saml2 import SAML2Provider, Attributes
 
-from .views import (
-    OneLoginSAML2ConfigureView, SelectIdP
-)
-
-from .constants import ONELOGIN_EMAIL, ONELOGIN_DISPLAYNAME
+from .views import SelectIdP
 
 
 class OneLoginSAML2Provider(SAML2Provider):
     name = 'OneLogin'
 
-    def get_configure_view(self):
-        return OneLoginSAML2ConfigureView.as_view()
+    def get_saml_setup_pipeline(self):
+        return [SelectIdP()]
 
-    def get_setup_pipeline(self):
-        return [
-            SelectIdP()
-        ]
-
-    def build_config(self, state):
-        data = super(OneLoginSAML2Provider, self).build_config(state)
-
-        if data:
-            data['attribute_mapping'] = {
-                'attribute_mapping_email': ONELOGIN_EMAIL,
-                'attribute_mapping_displayname': ONELOGIN_DISPLAYNAME,
-            }
-        return data
+    def attribute_mapping(self):
+        return {
+            Attributes.IDENTIFIER: 'PersonImmutableID',
+            Attributes.USER_EMAIL: 'User.email',
+            Attributes.FIRST_NAME: 'User.FirstName',
+            Attributes.LAST_NAME: 'User.LastName',
+        }
