29239e7cc397ae23a0a44ceeb638dc93e8f13d2d
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-10-09 16:01:28 -0700

    Add extra setup step / fillout attrs

--- src/sentry_auth_saml2/rippling/provider.py
+++ src/sentry_auth_saml2/rippling/provider.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, print_function
 
 from django import forms
 
+from sentry.auth.view import AuthView
 from sentry.auth.providers.saml2 import SAML2Provider, Attributes
 from sentry_auth_saml2.views import make_simple_setup
 from sentry_auth_saml2.forms import URLMetadataForm
@@ -18,16 +19,31 @@ SelectIdP = make_simple_setup(
 )
 
 
+class WaitForCompletion(AuthView):
+    """
+    Rippling provides the Metadata URL during initial application setup, before
+    configuration values have been saved, thus we cannot immediately attempt to
+    create an identity for the setting up the SSO.
+
+    This is simply an extra step to wait for them to complete that.
+    """
+    def handle(self, request, helper):
+        if 'continue_setup' in request.POST:
+            return helper.next_step()
+
+        return self.respond('sentry_auth_rippling/wait-for-completion.html')
+
+
 class RipplingSAML2Provider(SAML2Provider):
     name = 'Rippling'
 
     def get_saml_setup_pipeline(self):
-        return [SelectIdP()]
+        return [SelectIdP(), WaitForCompletion()]
 
     def attribute_mapping(self):
         return {
-            Attributes.IDENTIFIER: 'TODO',
-            Attributes.USER_EMAIL: 'TODO',
-            Attributes.FIRST_NAME: 'TODO',
-            Attributes.LAST_NAME: 'TODO',
+            Attributes.IDENTIFIER: 'user_id',
+            Attributes.USER_EMAIL: 'urn:oid:1.2.840.113549.1.9.1.1',
+            Attributes.FIRST_NAME: 'first_name',
+            Attributes.LAST_NAME: 'last_name',
         }
