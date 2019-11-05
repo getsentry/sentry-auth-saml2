b8e17389218e85aabee3857ddcd80395d199cf30
josh <josh@jrl.ninja>
2019-11-05 14:11:58 -0800

    py3(django): use AppConfig to defer model imports via sentry.auth (#47)

--- src/sentry_auth_saml2/generic/__init__.py
+++ src/sentry_auth_saml2/generic/__init__.py
@@ -1,7 +1,16 @@
 from __future__ import absolute_import
 
-from sentry.auth import register
+import django
 
-from .provider import GenericSAML2Provider
+if django.VERSION[:2] >= (1, 8):
+    # Django 1.9 specifically requires that models not be imported before setup,
+    # which sentry.auth does, so we need to use AppConfig here.
+    # Also works on 1.8.
+    default_app_config = "sentry_auth_saml2.generic.apps.Config"
+else:
+    # Provide backwards compatibility.
+    from sentry.auth import register
 
-register('saml2', GenericSAML2Provider)
+    from .provider import GenericSAML2Provider
+
+    register('saml2', GenericSAML2Provider)
--- src/sentry_auth_saml2/okta/__init__.py
+++ src/sentry_auth_saml2/okta/__init__.py
@@ -1,7 +1,16 @@
 from __future__ import absolute_import
 
-from sentry.auth import register
+import django
 
-from .provider import OktaSAML2Provider
+if django.VERSION[:2] >= (1, 8):
+    # Django 1.9 specifically requires that models not be imported before setup,
+    # which sentry.auth does, so we need to use AppConfig here.
+    # Also works on 1.8.
+    default_app_config = "sentry_auth_saml2.okta.apps.Config"
+else:
+    # Provide backwards compatibility.
+    from sentry.auth import register
 
-register('okta', OktaSAML2Provider)
+    from .provider import OktaSAML2Provider
+
+    register('okta', OktaSAML2Provider)
